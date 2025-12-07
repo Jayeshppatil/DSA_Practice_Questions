4dc2ce1465532163d26f0c7225aaf0869f1f9c0c
Jayesh Patil <jayeshpatil5819297@gmail.com>
2025-12-07 16:02:58 +0530

    Remove .vs folder and apply .gitignore

---
+++ TestApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] nums = { 0, 1, 0, 3, 12 };
+        MoveZeroes(nums);
+        Console.WriteLine(string.Join(", ", nums));
+    }
+
+    public static void MoveZeroes(int[] nums)
+    {
+        int n = nums.Length;
+        int start = 0;
+        int end = n - 1;
+
+        if (n > 1)
+        {
+            while (start < end)
+            {
+                if (nums[start] == 0)
+                {
+                    // Find the next non-zero from the end
+                    while (nums[end] == 0 && end > start)
+                    {
+                        end--;
+                    }
+                    if (start < end)
+                    {
+                        // Swap the zero with the non-zero value
+                        int temp = nums[start];
+                        nums[start] = nums[end];
+                        nums[end] = temp;
+                        end--; // move the 'end' pointer to the left
+                    }
+                }
+                start++; // move the 'start' pointer to the right
+            }
+        }
+    }
+
+}
